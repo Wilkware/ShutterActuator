25d01a74633e51f54698b034e49e4f9526757dc7
madrosDK <danielkoglek@gmx.at>
2025-03-25 15:13:37 +0100

    Update module.php

--- ShutterActuator/module.php
+++ ShutterActuator/module.php
@@ -176,7 +176,7 @@ class xcomfortshutter extends IPSModule
         if ($vid != 0) {
             $this->SendDebug(__FUNCTION__, 'Lower shutter!');
             //RequestAction($vid, 0.0);
-            RequestAction($vid, 2);
+            RequestAction($vid, 1);
         } else {
             $this->SendDebug(__FUNCTION__, 'Variable to control the shutter not set!');
         }
